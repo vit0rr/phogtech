585ff7adc9e2428bee06b307fc5e94ea1e60b7a1
Vitor <vi.souza.almeida@protonmail.com>
2021-07-18 18:44:01 -0300

    Home

---
+++ my-app/pages/_app.js
@@ -0,0 +1,210 @@
+import { createGlobalStyle, ThemeProvider } from 'styled-components'
+import 'bootstrap/dist/css/bootstrap.min.css';
+const GlobalStyle = createGlobalStyle`
+  html, body {
+    min-height: 100%;
+}
+
+body {
+    padding: 0;
+    margin: 0;
+}
+
+p {
+    font-size: 18px;
+}
+
+#GridProjects {
+    margin-top: 20px;
+}
+
+#titleServicos {
+    text-decoration: underline #FFAD00;
+}
+
+#services {
+    padding: 70px;
+    background-color: #ffedc7;
+}
+
+#projects {
+    padding: 70px;
+}
+
+.centralizar {
+    text-align: center;
+}
+
+/*Landingpage*/
+.centerHome {
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50% );
+    width: 100%;
+}
+
+#btnConhecer {
+    background-color: #FFAD00;
+    color: white;
+    font-weight: bold;
+    border: 0px;
+}
+
+.underlineAmarelo {
+    text-decoration: underline #FFAD00;
+}
+
+/*navbar*/
+ul > li > a:hover{
+    text-decoration: underline #FFAD00 2px;
+    transition: 0.3s;
+    transform: translateX(0) scale(1.1);
+}
+
+nav {
+    border-bottom: 2px solid #FFAD00;
+}
+
+#contatovH {
+    height: 100vh;
+}
+
+#contact {
+    border-radius: 5px;
+    height: 100vh;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    background-color: #e0d2bd79;
+}
+
+div.row > div.col-md-8 > p > a {
+    text-decoration: underline #FFAD00 2px;
+}
+
+div.container > div#projects > div#GridProjects > div.row > div.col-sm-6 {
+    margin-top: 5px;
+}
+
+/*Footer*/
+footer {
+    background-color: #FFAD00;
+    text-align: center;
+    color: white;
+    font-size: 17px;
+    padding: 10px;
+}
+
+#footerIcons > a > i{
+    padding: 5px;
+    font-size: 25px;
+    color: white;
+}
+
+div.row > div.col-sm > span#footerCopy {
+    font-weight: 500;
+}
+
+div.row > div.col-sm > span#devPor {
+    font-weight: 500;
+}
+
+div.row > div.col-sm > span#devName {
+    text-decoration: underline;
+}
+
+#borderContact {
+    border-right: 1px solid black;
+}
+
+/* Extra small devices (portrait phones, less than 576px)*/
+@media (max-width: 575.98px) { 
+    #services {
+        padding: 0px;
+        background-color: white;
+    }
+
+    #projects {
+        padding: 0px;
+        background-color: white;
+    }
+
+    #responsiveSvg {
+        display: none;
+    }
+
+    #borderContact {
+        border-right: 0px solid black;
+    }
+ }
+
+/* Small devices (landscape phones, 576px and up)*/
+@media (min-width: 576px) and (max-width: 767.98px) { 
+    #projects {
+        padding: 0px;
+        background-color: white;
+    }
+
+    #services {
+        padding: 0px;
+        background-color: white;
+    }
+
+    #responsiveSvg {
+        display: none;
+    }
+
+    #borderContact {
+        border-right: 0px solid black;
+    }
+ }
+
+/* Medium devices (tablets, 768px and up)*/
+@media (min-width: 768px) and (max-width: 991.98px) { 
+    #projects {
+        padding: 0px;
+        background-color: white;
+    }
+
+    #services {
+        padding: 0px;
+        background-color: white;
+    }
+
+    #responsiveSvg {
+        display: none;
+    }
+
+    #borderContact {
+        border-right: 0px solid black;
+    }
+ }
+
+/* Large devices (desktops, 992px and up)*/
+@media (min-width: 992px) and (max-width: 1199.98px) { 
+    /*nhe*/
+ }
+
+/* Extra large devices (large desktops, 1200px and up) */
+@media (min-width: 1200px) { 
+    /*nhe*/
+ }
+`
+
+const theme = {
+  colors: {
+    primary: '#0070f3',
+  },
+}
+
+export default function App({ Component, pageProps }) {
+  return (
+    <>
+      <GlobalStyle />
+      <ThemeProvider theme={theme}>
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </>
+  )
+}
---
+++ my-app/pages/index.js
@@ -0,0 +1,59 @@
+import styled from 'styled-components'
+import Head from 'next/head'
+import Image from 'next/image'
+import devHomepage from '../assets/img/devHomepage.svg'
+
+export default function Home() {
+  
+  return (
+    <div>
+      <Head>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.bundle.min.js"
+    integrity="sha384-gtEjrD/SeCtmISkJkNUaaKMoLD0//ElJ19smozuHV6z3Iehds+3Ulb9Bn9Plx0x4"
+    crossorigin="anonymous"></script>
+    <title>Pai com mel</title>
+</Head>
+    <nav className="navbar navbar-expand-lg navbar-light bg-light fixed-top">
+    <div className="container">
+      <a className="navbar-brand" href="#"><img src="https://raw.githubusercontent.com/vit0rr/phogtech/c73b17ffe2b84060b10e71f28df636e5c2d3b061/assets/img/logoPhogSvg.svg" alt="logo" width="65px"/> Phog Tech</a>
+      <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
+        aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span className="navbar-toggler-icon"></span>
+      </button>
+      <div className="collapse navbar-collapse" id="navbarNav">
+        <ul className="navbar-nav ms-auto">
+          <li className="nav-item">
+            <a className="nav-link active" aria-current="page" href="#">Home</a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link" href="./nos.html">Serviços - Projetos</a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link" href="./contato.html">Contato</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+  <div class="centerHome">
+    <div class="page-header-ui-content pt-5">
+      <div class="container px-5">
+        <div class="row gx-5 align-items-center">
+          <div class="col-lg-6">
+            <h1><span class="underlineAmarelo">Novas</span> soluções para novos tempos</h1>
+            <p class="page-header-ui-text mb-5">Bem vindo a Phog Tech Solutions. Aqui, vamos te ajudar com o que
+              precisa! Nossa equipe é especializada em desenvolvimento web. Somos uma boa escolha caso queira seu site
+              funcionando com agilidade.</p>
+            <a class="btn btn-lg btn-primary fw-500 me-3" id="btnConhecer" href="./nos.html">
+              Conhecer
+            </a>
+          </div>
+          <div class="col-lg-6 d-none d-lg-block"><Image
+              class="img-fluid" src={devHomepage}/></div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+  )
+}
