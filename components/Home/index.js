772448f1d5d85d8631bf75278ba4cbb4e1fb1703
Vitor <vi.souza.almeida@protonmail.com>
2021-07-31 23:15:52 -0300

    feat: página do index

---
+++ components/Home/index.js
@@ -0,0 +1,29 @@
+import Link from 'next/link'
+import Image from 'next/image'
+import devHomepage from '../../assets/img/devHomepage.svg'
+
+export default function HomeIndex() {
+    return (
+        <div className="centerHome">
+            <div className="page-header-ui-content pt-5">
+                <div className="container px-5">
+                    <div className="row gx-5 align-items-center">
+                        <div className="col-lg-6">
+                            <h1><span className="underlineAmarelo">Novas</span> soluções para novos tempos</h1>
+                            <p className="page-header-ui-text mb-5">Bem vindo a Phog Tech Solutions. Aqui, vamos te ajudar com o que
+                                precisa! Nossa equipe é especializada em desenvolvimento web. Somos uma boa escolha caso queira seu site
+                                funcionando com agilidade.</p>
+                            <Link href="/projects-services">
+                                <a className="btn btn-lg btn-primary fw-500 me-3" id="btnConhecer">
+                                    Conhecer
+                                </a>
+                            </Link>
+                        </div>
+                        <div className="col-lg-6 d-none d-lg-block"><Image
+                            className="img-fluid" src={devHomepage} /></div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
