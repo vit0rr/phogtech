7b1a18599e19e6cdf069b3baa84aea7daa71ec00
Vitor <vi.souza.almeida@protonmail.com>
2021-08-07 20:59:11 -0300

    refactor: mudança de async para defer para o JS do bootstrap

--- components/HeadContent/HeadContact.js
+++ components/HeadContent/HeadContact.js
@@ -3,7 +3,7 @@ import Head from 'next/head'
 export default function HeadContentContact() {
     return (
         <Head>
-            <script async src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.bundle.min.js"
+            <script defer src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.bundle.min.js"
                 integrity="sha384-gtEjrD/SeCtmISkJkNUaaKMoLD0//ElJ19smozuHV6z3Iehds+3Ulb9Bn9Plx0x4"
                 crossorigin="anonymous"></script>
             <link async href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet"
