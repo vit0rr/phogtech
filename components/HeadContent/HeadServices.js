a9ea535960401b759c6fb48c7288a7c7d7bf6e1e
Vitor <vi.souza.almeida@protonmail.com>
2021-08-07 21:05:00 -0300

    feat: adição de preload no CSS

--- components/HeadContent/HeadServices.js
+++ components/HeadContent/HeadServices.js
@@ -6,7 +6,7 @@ export default function HeadContentServices() {
             <script defer src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.bundle.min.js"
                 integrity="sha384-gtEjrD/SeCtmISkJkNUaaKMoLD0//ElJ19smozuHV6z3Iehds+3Ulb9Bn9Plx0x4"
                 crossorigin="anonymous"></script>
-            <link async href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet"
+            <link async rel="preload" href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet"
                 integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous"></link>
             <title>Phog Tech - Projetos e Serviços</title>
             <link rel="shortcut icon" href="https://raw.githubusercontent.com/vit0rr/phogtech/32491c044ce4fe189378b41eb391d30cfd2fee02/assets/img/logoPhogSvg.svg" type="image/x-icon"></link>
