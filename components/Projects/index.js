02e806a871eaecd1ba7aa9d48f8608e5caf8e7c9
Vitor <vi.souza.almeida@protonmail.com>
2021-09-11 09:06:28 -0300

    refactor: remoção de código e imports antigos

--- components/Projects/index.js
+++ components/Projects/index.js
@@ -1,9 +1,5 @@
-import Link from 'next/link'
 import Image from 'next/image'
 import MakerLauch from '../../assets/img/maker_launch.svg'
-import Koisinhas from '../../assets/img/portfolio/koisinhasdakisy.png'
-import MusaDaMente from '../../assets/img/portfolio/musadamente.png'
-import BrilhoInfancia from '../../assets/img/portfolio/brilhodainfancia.png'
 
 export default function Projects() {
     return (
@@ -25,68 +21,6 @@ export default function Projects() {
                             </div>
                         </div>
                     </div>
-                    {/* <div className="row">
-                <div className="col-sm-6">
-                    <div className="card rounded-0 bg-dark">
-                        <div className="card-body text-light">
-                            <div className="row">
-                                <div className="col-sm-6">
-                                    <Image src={Koisinhas}/> <br />
-                                    <a>
-                                    </a>
-                                </div>
-                                <div className="col-sm-6">
-                                    <h5 className="card-title">Koisinhas da Kisy</h5>
-                                    <p className="card-text">Construção de um e-commerce, manutenção de bugs e SEO.</p>
-                                    <a className="btn btn-md btn-primary fw-500 me-3" id="btnConhecer" href="https://www.koisinhasdakisy.com.br/">
-                                        Conhecer
-                                    </a>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <br/>
-                    <div className="card rounded-0 bg-dark">
-                        <div className="card-body text-light">
-                            <div className="row">
-                                <div className="col-sm-6">
-                                    <Image src={MusaDaMente}/> 
-                                    <br />
-                                    <a>
-                                    </a>
-                                </div>
-                                <div className="col-sm-6">
-                                    <h5 className="card-title">Musa da Mente</h5>
-                                    <p className="card-text">Desenvolvimento de um teste de personalidade. Para mais informações <Link href="https://github.com/Phog-Tech/musa-da-mente"><a>clique aqui</a></Link></p>
-                                    <a className="btn btn-md btn-primary fw-500 me-3" id="btnConhecer" href="https://musadamente-com-br.umbler.net/">
-                                        Conhecer
-                                    </a>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div className="col-sm-6">
-                    <div className="card rounded-0 bg-dark">
-                        <div className="card-body text-light">
-                            <div className="row">
-                                <div className="col-sm-6">
-                                    <Image src={BrilhoInfancia}/> <br />
-                                    <a>
-                                    </a>
-                                </div>
-                                <div className="col-sm-6">
-                                    <h5 className="card-title">Brilho da Infância</h5>
-                                    <p className="card-text">Desenvolvimento e manutenção do e-commerce, como entrega e pagamento.</p>
-                                    <a className="btn btn-md btn-primary fw-500 me-3" id="btnConhecer" href="https://www.brilhodainfancia.com.br/">
-                                        Conhecer
-                                    </a>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div> */}
                 </div>
             </div>
             <div class="row pt-2">
